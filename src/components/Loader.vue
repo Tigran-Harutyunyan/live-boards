cf4ff669928ba6ce23aa341091a7886cbb6181e7
Tigran <harutyunyan.tigran1975@gmail.com>
2024-12-07 00:07:23 +0400

    created Loader component

---
+++ src/components/Loader.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import LogoIcon from "@/components/icons/LogoIcon.vue";
+</script>
+
+<template>
+  <div class="h-full w-full flex flex-col justify-center items-center">
+    <component
+      :is="LogoIcon"
+      alt="Logo"
+      :width="120"
+      :height="120"
+      class="animate-pulse duration-700"
+    />
+  </div>
+</template>
