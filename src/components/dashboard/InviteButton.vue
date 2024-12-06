bc7ae39dce23ec778e6274a81997be6dd8edc035
Tigran <harutyunyan.tigran1975@gmail.com>
2024-12-07 00:08:55 +0400

    created InviteButton component to trigger  Clerk organization Profile

---
+++ src/components/dashboard/InviteButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import { OrganizationProfile } from "vue-clerk";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Plus } from "lucide-vue-next";
+</script>
+
+<template>
+  <Dialog>
+    <DialogTrigger as-child>
+      <Button variant="outline">
+        <Plus class="h-4 w-4 mr-2" />
+        Invite members
+      </Button>
+    </DialogTrigger>
+    <DialogContent class="p-0 bg-transparent border-none max-w-[880px]">
+      <OrganizationProfile />
+    </DialogContent>
+  </Dialog>
+</template>
