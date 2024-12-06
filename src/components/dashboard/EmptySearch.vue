403aca5ec10d24a086b2101501809f9cbcafbc7a
Tigran <harutyunyan.tigran1975@gmail.com>
2024-12-07 00:20:02 +0400

    Created EmptySearch component

---
+++ src/components/dashboard/EmptySearch.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import imgSearch from "@/assets/images/empty-search.svg";
+</script>
+
+<template>
+  <div class="h-full flex flex-col items-center justify-center">
+    <img :src="imgSearch" height="140" width="140" alt="Empty" />
+    <h2 class="text-2xl font-semibold mt-6">No results found!</h2>
+    <p class="text-muted-foreground textg-sm mt-2">
+      Try searching for something else
+    </p>
+  </div>
+</template>
