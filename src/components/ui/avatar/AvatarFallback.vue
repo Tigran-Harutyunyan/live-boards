ccbc53b266e24efde1b0429fdb9509a11be6fa97
Tigran <harutyunyan.tigran1975@gmail.com>
2024-12-07 00:05:39 +0400

    imported shadcn components via CLI

---
+++ src/components/ui/avatar/AvatarFallback.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { AvatarFallback, type AvatarFallbackProps } from "radix-vue";
+
+const props = defineProps<AvatarFallbackProps>();
+</script>
+
+<template>
+  <AvatarFallback
+    v-bind="props"
+    class="flex h-full w-full items-center justify-center rounded-md bg-sky-500 text-white"
+  >
+    <slot />
+  </AvatarFallback>
+</template>
---
+++ src/components/ui/button/Button.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from "vue";
+import { cn } from "@/lib/utils";
+import { Primitive, type PrimitiveProps } from "radix-vue";
+import { type ButtonVariants, buttonVariants } from ".";
+import { Loader2 } from "lucide-vue-next";
+
+interface Props extends PrimitiveProps {
+  variant?: ButtonVariants["variant"];
+  size?: ButtonVariants["size"];
+  class?: HTMLAttributes["class"];
+  isLoading?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  as: "button",
+});
+</script>
+
+<template>
+  <Primitive
+    :as="as"
+    :as-child="asChild"
+    :class="cn(buttonVariants({ variant, size }), props.class)"
+  >
+    <Loader2 v-if="isLoading" class="w-4 h-4 mr-2 animate-spin" />
+    <slot v-else />
+  </Primitive>
+</template>
