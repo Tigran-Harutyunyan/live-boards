aff5106a16acd3ee62c6e02e9da8f2869bcda0aa
Tigran <harutyunyan.tigran1975@gmail.com>
2024-12-07 00:36:25 +0400

    created ColorPicker component that shows color pallet

---
+++ src/components/board/ColorButton.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { Color } from "@/types/canvas";
+import { colorToCss } from "@/lib/utils";
+
+type EmitType = {
+  (event: "select", color: Color): void;
+};
+
+const emit = defineEmits<EmitType>();
+
+interface ColorButtonProps {
+  color: Color;
+}
+
+defineProps<ColorButtonProps>();
+</script>
+
+<template>
+  <button
+    class="w-8 h-8 items-center flex justify-center hover:opacity-75 transition"
+    @click="emit('select', color)"
+  >
+    <div
+      class="h-8 w-8 rounded-md border border-neutral-300"
+      :style="{ background: colorToCss(color) }"
+    />
+  </button>
+</template>
---
+++ src/components/board/ColorPicker.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import ColorButton from "./ColorButton.vue";
+import { Color } from "@/types/canvas";
+
+type EmitType = {
+  (event: "change", color: Color): void;
+};
+
+const emit = defineEmits<EmitType>();
+</script>
+
+<template>
+  <div
+    class="flex flex-wrap gap-2 items-center max-w-[164px] pr-2 mr-2 border-r border-neutral-200"
+  >
+    <ColorButton
+      :color="{ r: 243, g: 82, b: 35 }"
+      @select="(color) => emit('change', color)"
+    />
+    <ColorButton
+      :color="{ r: 255, g: 249, b: 177 }"
+      @select="(color) => emit('change', color)"
+    />
+    <ColorButton
+      :color="{ r: 68, g: 202, b: 99 }"
+      @select="(color) => emit('change', color)"
+    />
+    <ColorButton
+      :color="{ r: 39, g: 142, b: 237 }"
+      @select="(color) => emit('change', color)"
+    />
+    <ColorButton
+      :color="{ r: 155, g: 105, b: 245 }"
+      @select="(color) => emit('change', color)"
+    />
+    <ColorButton
+      :color="{ r: 252, g: 142, b: 42 }"
+      @select="(color) => emit('change', color)"
+    />
+    <ColorButton
+      :color="{ r: 0, g: 0, b: 0 }"
+      @select="(color) => emit('change', color)"
+    />
+    <ColorButton
+      :color="{ r: 255, g: 255, b: 255 }"
+      @select="(color) => emit('change', color)"
+    />
+  </div>
+</template>
