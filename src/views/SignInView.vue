6a7509f0dcea960f97a493508828b1de576fd423
Tigran <harutyunyan.tigran1975@gmail.com>
2024-12-06 23:53:25 +0400

    created sign-in and sign-up views

---
+++ src/views/SignInView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { SignIn } from "vue-clerk";
+</script>
+
+<template>
+  <SignIn sign-up-url="/sign-up" />
+</template>
---
+++ src/views/SignUpView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { SignUp } from "vue-clerk";
+</script>
+
+<template>
+  <SignUp sign-in-url="/sign-in" />
+</template>
